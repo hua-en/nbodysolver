d6a47ec7e43bace28d0572a9477726c050b87d97
shawn1368 <huaen.lim@gmail.com>
2023-10-17 10:11:20 +0800

    Rename ndarray_float

--- nbodysolver-py/python/nbodysolver/nbodysolver.pyi
+++ nbodysolver-py/python/nbodysolver/nbodysolver.pyi
@@ -1,25 +1,25 @@
 import numpy as np
 import numpy.typing as npt
 
-type ndarray_float = npt.NDArray[np.float64]
+type NDArrayFloat = npt.NDArray[np.float64]
 
 def solve_lorenz(coords: tuple[float, float, float], max_time: float, dt: float, 
                  tau: float=10.0, rho: float=28.0, beta: float=8.0/3.0) -> \
-                tuple[ndarray_float, ndarray_float, 
-                      ndarray_float, ndarray_float]: ...
+                tuple[NDArrayFloat, NDArrayFloat, 
+                      NDArrayFloat, NDArrayFloat]: ...
                 
-def simulate_nbody_and_process(r_list: ndarray_float, 
-                               v_list: ndarray_float, 
-                               m_list: ndarray_float, 
+def simulate_nbody_and_process(r_list: NDArrayFloat, 
+                               v_list: NDArrayFloat, 
+                               m_list: NDArrayFloat, 
                                dt: float, max_time: float, g: float=6.6743e-11) -> \
-    tuple[ndarray_float, ndarray_float, ndarray_float, 
-          ndarray_float, ndarray_float, ndarray_float]: ...
+    tuple[NDArrayFloat, NDArrayFloat, NDArrayFloat, 
+          NDArrayFloat, NDArrayFloat, NDArrayFloat]: ...
     
-def total_energy_nbody(r_list: ndarray_float, 
-                       v_list: ndarray_float, 
-                       m_list: ndarray_float, g: float=6.6743e-11) -> \
+def total_energy_nbody(r_list: NDArrayFloat, 
+                       v_list: NDArrayFloat, 
+                       m_list: NDArrayFloat, g: float=6.6743e-11) -> \
     tuple[float, float, float]: ...
     
-def all_planet_acc_nbody(r_list: ndarray_float, 
-                         m_list: ndarray_float, g: float=6.6743e-11) -> \
-    ndarray_float: ...
+def all_planet_acc_nbody(r_list: NDArrayFloat, 
+                         m_list: NDArrayFloat, g: float=6.6743e-11) -> \
+    NDArrayFloat: ...
