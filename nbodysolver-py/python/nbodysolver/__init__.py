40774963d853f533abdfd99f273e9bb095a67ba4
shawn1368 <huaen.lim@gmail.com>
2023-10-17 10:13:57 +0800

    Add pytest and mypy

--- nbodysolver-py/python/nbodysolver/__init__.py
+++ nbodysolver-py/python/nbodysolver/__init__.py
@@ -3,6 +3,7 @@ from nbodysolver.plotting import *
 from nbodysolver.example_systems import *
 
 
+print(nbodysolver.__module__)
 __doc__ = nbodysolver.__doc__ # type: ignore
 if hasattr(nbodysolver, "__all__"): # type: ignore
     __all__ = nbodysolver.__all__ # type: ignore
